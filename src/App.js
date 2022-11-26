8331a042d42320c8a3e3949a102113d39043d6cb
Pete <petr.stepanenko@gmail.com>
2022-11-26 10:38:33 -0600

    app tracks clicks within a board and tracks the position of the board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {state,useEffect,useState} from 'react';
 
 function App() {
+
+const [clickCoordinates, setClickCoordinates]=useState({x:-1,y:-1});
+const [boardCoordinates, setBoardCoordinates]=useState({x:-1,y:-1});
+
+
+function handleClick(e){
+  setClickCoordinates({x: e.screenX, y: e.screenY});
+}
+
+useEffect(()=>{ 
+  const board = document.querySelector("#board");
+  const boardPosition = board.getBoundingClientRect();
+  console.log(JSON.stringify(boardPosition));
+  setBoardCoordinates({x:boardPosition.left, y:boardPosition.top});
+
+  board.addEventListener('click',handleClick);
+  return ()=> document.removeEventListener('click',handleClick);
+},[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <svg id="board"  xmlns="http://www.w3.org/2000/svg" width="500px" height="500px">
+      <g fill="white" stroke="black" strokeWidth="5">
+        <rect width="100%" height="100%"/>
+      </g>
+    </svg>
+    <p>
+      Click: x= {clickCoordinates.x}, y= {clickCoordinates.y};
+      <br />
+      Board: x= {boardCoordinates.x}, y= {boardCoordinates.y};
+      <br />
+      
+    </p>
+    </>
   );
 }
 
