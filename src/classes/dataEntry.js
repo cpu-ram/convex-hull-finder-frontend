285a33eabde51f50171b72260c14f9c071202e2d
Pete <petr.stepanenko@gmail.com>
2022-12-03 12:35:52 -0600

    seemingly fixed DOM updates with Jason's help

--- src/classes/dataEntry.js
+++ src/classes/dataEntry.js
@@ -2,9 +2,7 @@ import { cleanup } from '@testing-library/react';
 import {useEffect,useState} from 'react';
 
 function DataEntry() {
-  
-
-  const [pointsEntered, setPointsEntered]=useState([]);
+  const [pointsEntered, setPointsEntered] = useState([]);
 
   function PointsDisplayed(props){
     return(
@@ -14,45 +12,76 @@ function DataEntry() {
     )
   }
 
-  useEffect(()=>{ 
-    function handleClick(e){
+  function handleClick(e) {
+    console.log("e", e);
   
-    let enteredViewPortCoordinates={x:e.clientX, y:e.clientY};
+    let enteredViewPortCoordinates={ x: e.clientX, y: e.clientY };
+    let boardPosition=document.querySelector('#board');
+    setPointsEntered([...pointsEntered, enteredViewPortCoordinates]);
     
-    const boardPosition = board.getBoundingClientRect();
+    // const boardPosition = board.getBoundingClientRect();
     
-    let boardRelativeClickCoordinates={
-      x:enteredViewPortCoordinates.x-boardPosition.left,
-      y:enteredViewPortCoordinates.y-boardPosition.top
-    }
-    pointsEntered.push(boardRelativeClickCoordinates);
-    console.log(JSON.stringify(pointsEntered));
-    }
+    // let boardRelativeClickCoordinates={
+    //   x:enteredViewPortCoordinates.x-boardPosition.left,
+    //   y:enteredViewPortCoordinates.y-boardPosition.top
+    // }
+    // setPointsEntered([...pointsEntered, boardRelativeClickCoordinates])
+    // console.log("pointsEntered", pointsEntered)
+    // pointsEntered.push(boardRelativeClickCoordinates);
+    // setPointsEntered(pointsEntered);
+    // console.log(JSON.stringify(pointsEntered));
+  }
 
-    const board = document.querySelector("#board");
-    board.addEventListener('click',handleClick);
+  // useEffect(() => {
+  //   const board = document.querySelector("#board");
 
-    return function cleanup(){
-      board.removeEventListener('click', handleClick);
-    }
+  //   function handleClick(e) {
+  
+  //     let enteredViewPortCoordinates={x:e.clientX, y:e.clientY};
+      
+  //     const boardPosition = board.getBoundingClientRect();
+      
+  //     let boardRelativeClickCoordinates={
+  //       x:enteredViewPortCoordinates.x-boardPosition.left,
+  //       y:enteredViewPortCoordinates.y-boardPosition.top
+  //     }
+  //     setPointsEntered([...pointsEntered, boardRelativeClickCoordinates])
+  //     console.log("pointsEntered", pointsEntered)
+  //     // pointsEntered.push(boardRelativeClickCoordinates);
+  //     // setPointsEntered(pointsEntered);
+  //     // console.log(JSON.stringify(pointsEntered));
+  //   }
 
-  },[]);
+  //   board.addEventListener('click', handleClick);
+
+  //   return function cleanup(){
+  //     board.removeEventListener('click', handleClick);
+  //   }
+  // }, []);
 
   function FindConvexHull(){
 
   }
+
+  // console.log("pointsEntered", pointsEntered)
 
   return (
     <>
     <div style={{display:"block",boxSizing:"border-box",width:"530px",height:"530px",
       margin:"0", paddingTop:"15px", paddingRight:"15px",
         paddingLeft:"15px", paddingBottom:"15px", outline:"1px solid red"}}>
+        {pointsEntered.map((point=><circle cx={point.x} cy={point.y} r='5' /> ))}
 
-    <svg id="board"  xmlns="http://www.w3.org/2000/svg" width="500px" height="500px" 
+    <svg id="board" onClick={handleClick}  xmlns="http://www.w3.org/2000/svg" width="500px" height="500px" 
       style={{display: "block", margin:"0",
         boxSizing: "content-box", padding:"0", outline:"1px solid blue"}}>
       
-      <PointsDisplayed pointsEntered={pointsEntered}></PointsDisplayed>
+      {/* <PointsDisplayed pointsEntered={pointsEntered}> */}
+      
+      {pointsEntered.map((point=><circle cx={point.x} cy={point.y} r='5' /> ))}
+      <circle cx='20' cy='20' r='5' />
+
+      {/* </PointsDisplayed> */}
 
     </svg>
     </div>
